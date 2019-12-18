dd7bd92796fda4500b9d0a1b5180628839897aaa
liuyc <liuyc_liuyc@163.com>
2019-12-18 08:48:55 +0800

    init

---
+++ src/main/java/com/lyc/Start.java
@@ -0,0 +1,13 @@
+package com.lyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+@MapperScan(basePackages = "com.lyc.dao")
+public class Start {
+    public static void main(String[] args) {
+        SpringApplication.run(Start.class,args);
+    }
+}
